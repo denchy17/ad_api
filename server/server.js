5f697ad0fdea84116d7f9b3381af6c06993e42dd
chykalov <chykalov@cosmocode.de>
2024-05-26 17:12:54 +0200

    KAN-18 Connect database to backend

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,8 +7,21 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const dbOwner = process.env.DB_OWNER;
+const dbPassword = process.env.DB_PASSWORD;
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${dbOwner}:${dbPassword}@db1.x2w3yhm.mongodb.net/manager`);
+        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+    } catch (error) {
+        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+    }
+};
 
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
+  dbConnection();
   console.log(`Server running on port ${PORT}`);
 });
