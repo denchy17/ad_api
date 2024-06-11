122b2afc8b403eea9e1f8d8c019f45b7c7e1118c
chykalov <chykalov@cosmocode.de>
2024-06-11 17:53:55 +0200

    Improve DB connection

--- server/config/db.js
+++ server/config/db.js
@@ -5,15 +5,29 @@ dotenv.config();
 
 const dbOwner = process.env.DB_OWNER;
 const dbPassword = process.env.DB_PASSWORD;
-const MONGO_URI = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
+
+const maxRetries = 5;
+const retryDelay = 3000;
 
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://${dbOwner}:${dbPassword}@cluster0.e3dmxsw.mongodb.net/manager`);
-        // await mongoose.connect(MONGO_URI);
-        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
-    } catch (error) {
-        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+    let retries = maxRetries;
+
+    while (retries > 0) {
+        try {
+            await mongoose.connect(`mongodb+srv://${dbOwner}:${dbPassword}@${dbName}.e3dmxsw.mongodb.net/manager`, {
+            });
+            console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+            break;
+        } catch (error) {
+            retries -= 1;
+            console.error(`COULD NOT CONNECT TO DATABASE. Retrying in ${retryDelay / 1000} seconds...`, error.message);
+            if (retries > 0) {
+                await new Promise(res => setTimeout(res, retryDelay));
+            } else {
+                console.error('COULD NOT CONNECT TO DATABASE. Maximum retries reached:', error.message);
+            }
+        }
     }
 };
 
